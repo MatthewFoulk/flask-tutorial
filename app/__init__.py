d413dd98247532c9561a5c0d06f1165305a3531e
Matthew Foulk <mewuzhere12@gmail.com>
2021-01-01 21:27:17 -0500

    Added local time support

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -20,6 +21,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
